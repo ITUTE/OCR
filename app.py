59b0e797409b10a24fcd274639354aba6606e8a7
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-01-03 14:48:00 +0700

    ocr multi files

--- app.py
+++ app.py
@@ -1,37 +1,48 @@
-# import the necessary packagesgit
 from PIL import Image
 import pytesseract
 import cv2
 import os
 
-img_path = "news/news-1.png"
+folder = "doc"
+images = os.listdir(folder)
+filenames = [(filename.split("."))[0] for filename in images]
 
-# Đọc file ảnh 
-image = cv2.imread(img_path)
+for index, image in enumerate(images):
+    # Đọc file ảnh 
+    img_path = folder + "/" + image
+    image = cv2.imread(img_path)
+    # image = cv2.resize(image, (1350, 1150))
 
-# Chuyển ảnh màu sang ảnh gray
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Chuyển ảnh màu sang ảnh gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Lưu ảnh đã convert
-filename = "images/gray_img.png"
-cv2.imwrite(filename, gray)
+    # threshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+    # ret,threshold = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
+    # cv2.imwrite("images/threshold.png", threshold)
 
-# Load ảnh và apply nhận dạng bằng Tesseract OCR
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-text = pytesseract.image_to_string(Image.open(filename), lang='vie')
+    # Lưu ảnh đã convert tạm thời
+    filename = "images/gray_img.png"
+    cv2.imwrite(filename, gray)
 
-# Xóa ảnh tạm sau khi nhận dạng
-os.remove(filename) 
+    # Load ảnh và apply nhận dạng bằng Tesseract OCR
+    # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    text = pytesseract.image_to_string(Image.open(filename), lang='vie')
 
-# In dòng chữ nhận dạng được
-with open("out.txt", "w", encoding='utf8') as f:
-    print(text)
-    f.write(text)
+    # Xóa ảnh tạm sau khi nhận dạng
+    os.remove(filename) 
+
+    # In dòng chữ nhận dạng được
+    output = "output/" + filenames[index] + ".txt"
+    print(output)
+
+    with open(output, "w", encoding="utf8") as fw:
+        fw.write(text)
  
-# Hiển thị các ảnh chúng ta đã xử lý.
-cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
+print("finished")
+# # Hiển thị các ảnh chúng ta đã xử lý.
+# cv2.imshow("Image", image)
+# cv2.imshow("Output", gray)
 
-# Đợi chúng ta gõ phím bất kỳ
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# # Đợi chúng ta gõ phím bất kỳ
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
